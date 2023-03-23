fdee87657ce80b4465da227e0a6bbb05ee96b4df
CircleCI <office@datenkraft.com>
2023-03-23 10:38:20 +0000

    Autogenerated client update [publish]

---
+++ src/Generated/Exception/GetAuthPermissionCollectionForbiddenException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Datenkraft\Backbone\Client\SkuCatalogApi\Generated\Exception;
+
+class GetAuthPermissionCollectionForbiddenException extends ForbiddenException
+{
+    /**
+     * @var \Datenkraft\Backbone\Client\SkuCatalogApi\Generated\Model\ErrorResponse
+     */
+    private $errorResponse;
+    public function __construct(\Datenkraft\Backbone\Client\SkuCatalogApi\Generated\Model\ErrorResponse $errorResponse)
+    {
+        parent::__construct('Forbidden');
+        $this->errorResponse = $errorResponse;
+    }
+    public function getErrorResponse() : \Datenkraft\Backbone\Client\SkuCatalogApi\Generated\Model\ErrorResponse
+    {
+        return $this->errorResponse;
+    }
+}

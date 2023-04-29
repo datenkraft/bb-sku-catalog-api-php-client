f91945e492c4548e905da9ca711fd30f784a25c9
CircleCI <office@datenkraft.com>
2023-04-29 11:24:44 +0000

    Autogenerated client update [publish]

---
+++ src/Generated/Exception/DeleteAuthPermissionRoleCollectionBadRequestException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Datenkraft\Backbone\Client\SkuCatalogApi\Generated\Exception;
+
+class DeleteAuthPermissionRoleCollectionBadRequestException extends BadRequestException
+{
+    /**
+     * @var \Datenkraft\Backbone\Client\SkuCatalogApi\Generated\Model\ErrorResponse
+     */
+    private $errorResponse;
+    public function __construct(\Datenkraft\Backbone\Client\SkuCatalogApi\Generated\Model\ErrorResponse $errorResponse)
+    {
+        parent::__construct('Bad Request');
+        $this->errorResponse = $errorResponse;
+    }
+    public function getErrorResponse() : \Datenkraft\Backbone\Client\SkuCatalogApi\Generated\Model\ErrorResponse
+    {
+        return $this->errorResponse;
+    }
+}
